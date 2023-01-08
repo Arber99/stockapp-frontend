59df7068ba2e476142fd3670b3ff018c945d0f2d
Arber Iseni <arber-99@live.de>
2023-01-08 20:38:53 +0100

    ✅ Fixed test

--- src/app/pages/account/account.component.spec.ts
+++ src/app/pages/account/account.component.spec.ts
@@ -1,20 +1,20 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core";
-import { AccountComponent } from "./account.component";
+import { AccountPage } from "./account.component";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 describe("AccountComponent", () => {
 
-  let fixture: ComponentFixture<AccountComponent>;
-  let component: AccountComponent;
+  let fixture: ComponentFixture<AccountPage>;
+  let component: AccountPage;
   beforeEach(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
       ],
-      declarations: [AccountComponent]
+      declarations: [AccountPage]
     });
 
-    fixture = TestBed.createComponent(AccountComponent);
+    fixture = TestBed.createComponent(AccountPage);
     component = fixture.componentInstance;
 
   });
