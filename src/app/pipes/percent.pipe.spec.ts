1ccac91de1baf44fecb17d0eed33449fbd757a69
arber99 <arber-99@live.de>
2023-08-13 13:17:22 +0200

    🧪 Remove tests

--- src/app/pipes/percent.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PercentPipe } from './percent.pipe';
-
-describe('PercentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PercentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
